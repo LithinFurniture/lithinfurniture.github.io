754cfd0039f67f03f30384772a19884cd80c8e41
Bijin <kbijin528@gmail.com>
2021-04-24 12:28:51 +0530

    .js file added, navbar height decreased

---
+++ script.js
@@ -0,0 +1,12 @@
+// When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar-div").style.top = "0";
+    } else {
+        document.getElementById("navbar-div").style.top = "-250px";
+    }
+    prevScrollpos = currentScrollPos;
+}
